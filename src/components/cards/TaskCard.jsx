2903802de2f6588f222929b36235f2fc95fd7c68
Pkhrare <prakhar.khare@waivergroup.com>
2025-08-19 11:44:04 -0500

    Fix button in task card

--- src/components/cards/TaskCard.jsx
+++ src/components/cards/TaskCard.jsx
@@ -478,7 +478,7 @@ const TaskCard = ({ task, onClose, onTaskUpdate, assigneeOptions, isClientView =
                                                     ))}
                                                 </select>
                                             </div>
-                                            {!isClientView && <button type="button" className="p-2 border rounded-md hover:bg-gray-100"><svg className="w-6 h-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg></button>}
+                                            {/* {!isClientView && <button type="button" className="p-2 border rounded-md hover:bg-gray-100"><svg className="w-6 h-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg></button>} */}
                                         </div>
                                     </div>
                                     <div>
